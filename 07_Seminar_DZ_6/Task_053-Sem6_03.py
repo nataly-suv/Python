6fcb2b31aaab4687575c3e1e2d06da8fbe1a248e
Nataly Suvorova <suvorova.nata.a@gmail.com>
2022-10-11 17:31:27 +0300

    Семинар 6. ДЗ 6

---
+++ 07_Seminar_DZ_6/Task_053-Sem6_03.py
@@ -0,0 +1,27 @@
+# ===================================================
+# Преобразовать набор списков (используя функцию zip)
+''' users = ['user1','user2','user3','user4','user5']
+    ids = [4, 5, 9, 14, 7]
+    salary = [111,222,333] 
+'''
+# в другой набор 
+''' ['user1', 4, 111]
+    ['user2', 5, 222]
+    ['user3', 9, 333]
+'''
+# и потом вернуть в исходное состояние
+'''['user1', 'user2', 'user3']
+   [4, 5, 9]
+   [111, 222, 333]
+'''
+# ===================================================
+
+
+users = ['user1', 'user2', 'user3', 'user4', 'user5']
+ids = [4, 5, 9, 14, 7]
+salary = [111, 222, 333]
+a = list(zip(users, ids, salary))
+print(a)
+users, ids, salary = zip(*a)
+users, ids, salary = list(users), list(ids), list(salary)
+print(users, ids, salary)
